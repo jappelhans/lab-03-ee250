ddb5aef31a741d9865a13a36f55ce2aeedfdd904
jappelhans <appelhan@usc.edu>
2023-02-06 10:47:44 -0800

    weather client

---
+++ joke_client.py
@@ -0,0 +1,28 @@
+import requests
+from typing import Dict
+
+# connect to a "real" API
+
+## Example: Official Joke API
+URL = "https://official-joke-api.appspot.com/random_joke"
+
+def get_joke() -> Dict:
+    res = requests.get(URL)
+    return res.json()
+
+# TODO: try connecting to a another API! e.g. reddit (https://www.reddit.com/dev/api/)
+
+def main():
+
+    for i in range(0,5):
+        joke = get_joke()
+        print("Category: ", joke["type"])
+        print("Setup: ", joke["setup"])
+        print("Punchline: ", joke["punchline"])
+        print()
+
+
+
+if __name__ == "__main__":
+    main()
+
--- weather_client.py
+++ weather_client.py
@@ -7,17 +7,34 @@ from typing import Dict
 URL = "https://api.openweathermap.org/data/2.5/weather"
 
 # TODO: get an API key from openweathermap.org and fill it in here!
-API_KEY = ""
+API_KEY = "6504cd64f13bad0c6899c6f66a33a4c9"
 
 def get_weather(city) -> Dict:
     res = requests.get(URL, params={"q": city, "appid": API_KEY})
     return res.json()
 
+def KtoF(K):
+    F = 1.8*(K-273)+32
+    F = round(F,1)
+    return F
+
+
 # TODO: try connecting to a another API! e.g. reddit (https://www.reddit.com/dev/api/)
 
 def main():
-    temp = get_weather("London")
-    print(temp)
+    city = "Las Vegas"
+    temp = get_weather(city)
+    # print(temp)
+
+    data = temp["main"]
+
+    print("City: ", temp["name"], sep="")
+    print("Current temp: ", KtoF(data["temp"]), " ", chr(176), "F", sep="")
+    print("Feels like:   ", KtoF(data["feels_like"]), " ", chr(176), "F", sep="")
+    print("High today:   ", KtoF(data["temp_max"]), " ", chr(176), "F", sep="")
+    print("Low today:    ", KtoF(data["temp_min"]), " ", chr(176), "F", sep="")
+    print("Humidity:     ", data["humidity"], "%", sep="")
+
 
 if __name__ == "__main__":
     main()
